544a14d86dccd98ea30a9216137b89f304f4c2b3
Kimserey <kimserey.lam@gmail.com>
2019-02-09 11:22:37 +0000

    complete

--- y-combinator.rkt
+++ y-combinator.rkt
@@ -50,8 +50,53 @@
 ; as almost-factorial takes a function which is expected to be a "working" factorial,
 ; if we pass factorial, it will work since factorial is definitely working.
 
+; The Y none combinator - as it references itself in a recursive manner
 (define Y
   (λ (f)
     (f (λ (x) (Y f) x))))
 
 (define factorial/2 (Y almost-factorial))
+
+; Deriving the Y combinator
+
+(define (part-factorial self)
+  (let ((f (lambda (y) ((self self) y))))
+    (lambda (n)
+      (if (= n 0)
+          1
+          (* n (f (- n 1)))))))
+
+(define (part-factorial/2 self)
+  (almost-factorial
+   (λ (y) ((self self) y))))
+
+(define part-factorial/3
+  (λ (self)
+    (almost-factorial
+     (λ (y) ((self self) y)))))
+
+(define factorial/3 (part-factorial/2 part-factorial/2))
+
+(define factorial/4
+  (let ([pf (λ (self)
+              (almost-factorial
+               (λ (y) ((self self) y))))])
+    (pf pf)))
+
+(define factorial/5
+  ((λ (x) (x x))
+   (λ (x)
+     (almost-factorial (λ (y) ((x x) y))))))
+
+; Extracts the pattern within factorial/5 to make a general recursive pattern
+(define make-recursive
+  (λ (f)
+    ((λ (x) (x x))
+     (λ (x) (f (λ (y) ((x x) y)))))))
+
+(define Y-combinator
+  (λ (f)
+    ((λ (x) (x x))
+     (λ (x) (f (λ (y) ((x x) y)))))))
+
+(define factorial/6 (Y-combinator almost-factorial))
