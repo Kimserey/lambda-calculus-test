ade2a7563d47d90bdf7051857a9d730bec76beaa
Kimserey <kimserey.lam@gmail.com>
2019-02-09 10:40:30 +0000

    more

--- y-combinator.rkt
+++ y-combinator.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+; code from mvanier post:
+; https://mvanier.livejournal.com/2897.html
+
 (require racket/trace)
 
 (define factorial
@@ -8,5 +11,42 @@
         1
         (* n (factorial (- n 1))))))
 
-(trace factorial)
-(factorial 5)
+; (trace factorial)
+; (factorial 5)
+
+(define almost-factorial
+  (λ (f)
+    (λ (n)
+      (if (zero? n)
+          1
+          (* n (f (- n 1)))))))
+
+;(define factorial (Y almost-factorial))
+; with hypothetical factorial working
+
+(define identity (λ (x) x))
+
+(define factorialB (almost-factorial identity))
+
+(define factoiralB/1
+  (lambda (n)
+    (if (zero? n)
+        1
+        (* n (identity (- n 1))))))
+
+(define factorial0 (almost-factorial identity))
+
+;(factorial0 1)
+
+(define factorial1
+  (almost-factorial factorial0))
+
+(define factorial1/2
+  (almost-factorial
+   (almost-factorial identity)))
+
+; factorial is the fixpoint of almost-factorial because:
+; factorial = almost-factorial factorial
+; as almost-factorial takes a function which is expected to be a "working" factorial,
+; if we pass factorial, it will work since factorial is definitely working.
+
