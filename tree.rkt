e5659ed53acda19decb70aaaa3fb9a5c730bbb02
Kimserey <kimserey.lam@gmail.com>
2019-02-24 11:10:06 +0000

    simple node tree

--- tree.rkt
+++ tree.rkt
@@ -1,27 +1,30 @@
 #lang racket
 
-; Consider the follwoing Baskets of apples problem.
-;
-
-(define x 10)
-
 (require pict pict/tree-layout)
-(define (draw tree)
-  (define (viz tree)
-    (cond
-      ((null? tree) #f)
-      ((not (pair? tree))
-       (tree-layout #:pict (cc-superimpose
-                            (disk 50 #:color "gray")
-                            (text (symbol->string tree)))))
-      ((not (pair? (car tree)))
-       (apply tree-layout (map viz (cdr tree))
-              #:pict (cc-superimpose
-                      (disk 50 #:color "white")
-                      (text (symbol->string (car tree))))))))
-  (if (null? tree)
-      #f
-      (naive-layered (viz tree))))
 
-(define tree '(limit/80 (limit/50 basket/20 basket/10) (limit/50 basket/20 basket/10)))
-(draw tree)
+(define (circle t col children)
+  (tree-layout children #:pict (cc-superimpose (disk 50 #:color col) (text t))))
+
+(define (rectangle t col)
+  (tree-layout #:pict (cc-superimpose (filled-rectangle 100 50 #:color col) (text t))))
+
+(define tree (tree-layout ))
+
+(define (build-layout tree)
+  (cond
+    [(not (pair? tree))
+     (tree-layout #:pict (cc-superimpose
+                          (filled-rectangle 100 50 #:color "gray")
+                          (text (symbol->string tree))))]
+    [else
+     (apply tree-layout
+            #:pict (cc-superimpose
+                    (disk 70 #:color "white")
+                    (text (symbol->string (car tree))))
+            (map build-layout (cdr tree)))]))
+
+(define (draw tree) (naive-layered (build-layout tree)))
+
+(draw '(limit/50
+        (limit/100 basket/200 basket/100)
+        (limit/50 basket/100 basket/10)))
