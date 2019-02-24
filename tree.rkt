8a22ec383d100d3b1bfd7651118af558f84abdba
Kimserey <kimserey.lam@gmail.com>
2019-02-24 16:31:41 +0000

    More

--- tree.rkt
+++ tree.rkt
@@ -3,40 +3,34 @@
 (require pict pict/tree-layout)
 
 (define (tree-layout/titled node title subtitle child)
-    (apply tree-layout #:pict (cc-superimpose node (vc-append (text title) (text subtitle))) child))
+    (apply
+     tree-layout
+     #:pict (cc-superimpose node (vc-append (text title) (text subtitle)))
+     (map (λ (node) (tree-edge #:edge-width 3 #:edge-color "black" node)) child)))
 
 (define (c/o title subtitle [child '()])
-  (tree-layout/titled (disk 70 #:color "orange") title subtitle child))
+  (tree-layout/titled (disk 70 #:border-width 3 #:color "orange") title subtitle child))
 
 (define (c/w title subtitle [child '()])
-  (tree-layout/titled (disk 70 #:color "white") title subtitle child))
+  (tree-layout/titled (disk 70 #:border-width 3 #:color "white") title subtitle child))
 
 (define (r/o title subtitle [child '()])
-  (tree-layout/titled (filled-rectangle 150 50 #:color "orange") title subtitle child))
+  (tree-layout/titled (filled-rectangle 150 50 #:border-width 3 #:color "orange") title subtitle child))
 
 (define (r/g title subtitle [child '()])
-  (tree-layout/titled (filled-rectangle 150 50 #:color "gray") title subtitle child))
+  (tree-layout/titled (filled-rectangle 150 50 #:border-width 3 #:color "gray") title subtitle child))
 
-(naive-layered
-  (c/o "limit 1" "0"
-                (list
-                 (c/o "limit 2" "0"
-                      (list
-                       (r/o "basket 1" "limit: 0 | apples: 1")
-                       (r/g "basket 2" "limit: 0 | apples: 1")))
-                 (c/w "limit 3" "0"
-                      (list
-                       (r/g "basket 3" "limit: 0 | apples: 1")
-                       (r/g "basket 4" "limit: 0 | apples: 1"))))))
+(define pic 
+  (naive-layered
+   (c/w "limit 1" "15"
+        (list
+         (c/w "limit 2" "10"
+              (list
+               (r/g "basket A" "slots: 10 | apples: 0")
+               (r/g "basket B" "slots: 10 | apples: 0")))
+         (c/w "limit 3" "20"
+              (list
+               (r/g "basket C" "slots: 10 | apples: 0")
+               (r/g "basket D" "slots: 10 | apples: 0")))))))
 
-(naive-layered
-  (c/o "limit 1" "0"
-                (list
-                 (c/w "limit 2" "0"
-                      (list
-                       (r/g "basket 1" "limit: 0 | apples: 1")
-                       (r/g "basket 2" "limit: 0 | apples: 1")))
-                 (c/o "limit 3" "0"
-                      (list
-                       (r/g "basket 3" "limit: 0 | apples: 1")
-                       (r/o "basket 4" "limit: 0 | apples: 1"))))))
+(send (pict->bitmap pic) save-file "img\\example.png" 'png)
