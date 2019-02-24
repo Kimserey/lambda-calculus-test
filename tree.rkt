08152f5a40dd44f251952f59e312c0b46d4b4372
Kimserey <kimserey.lam@gmail.com>
2019-02-24 09:56:42 +0000

    tree

--- tree.rkt
+++ tree.rkt
@@ -1,3 +1,27 @@
 #lang racket
 
+; Consider the follwoing Baskets of apples problem.
+;
+
 (define x 10)
+
+(require pict pict/tree-layout)
+(define (draw tree)
+  (define (viz tree)
+    (cond
+      ((null? tree) #f)
+      ((not (pair? tree))
+       (tree-layout #:pict (cc-superimpose
+                            (disk 50 #:color "gray")
+                            (text (symbol->string tree)))))
+      ((not (pair? (car tree)))
+       (apply tree-layout (map viz (cdr tree))
+              #:pict (cc-superimpose
+                      (disk 50 #:color "white")
+                      (text (symbol->string (car tree))))))))
+  (if (null? tree)
+      #f
+      (naive-layered (viz tree))))
+
+(define tree '(limit/80 (limit/50 basket/20 basket/10) (limit/50 basket/20 basket/10)))
+(draw tree)
