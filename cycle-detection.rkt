127ddcf14cb049f18bab5dd4b174e30bc98d69f0
Kimserey <kimserey.lam@gmail.com>
2019-03-01 20:51:25 +0000

    more

--- cycle-detection.rkt
+++ cycle-detection.rkt
@@ -2,23 +2,45 @@
 
 (define nodes
   (list
-   (list 1 2)
-   (list 2 3)
-   (list 3 4)
-   (list 4 2)))
+   (cons 1 2)
+   (cons 2 3)
+   (cons 3 4)
+   (cons 4 2)
+   (cons 5 6)))
 
-(define (find-meeting next p1 p2)
-  (let ([slow (next p1)]
-        [fast (next (next p2))])
-    (cond
-      [(= slow fast) slow]
-      [else (find-meeting next slow fast)])))
-
-(define (get-linked-node parent)
+; Finds the child given a parent
+(define (next parent)
   (define (find nodes parent)
     (let* ([node (car nodes)]
            [p (car node)]
-           [c (cadr node)])
+           [c (cdr node)])
       (if (= parent p) c (find (cdr nodes) parent))))
   (find nodes parent))
-      
+
+(define (next/next x) (next (next x)))
+
+; Traverses a list at the speed specified
+(define (traverse slow fast p1 p2 steps)
+  (let ([p1 (slow p1)]
+        [p2 (fast p2)])
+    (cond
+      [(= p1 p2) (cons p1 steps)]
+      [else (traverse slow fast p1 p2 (+ 1 steps))])))
+
+; Extracts result from traverse
+(define (traverse/result slow fast p1 p2)
+  (car (traverse slow fast p1 p2 0)))
+
+; Extracts the count of iteration from traverse 
+(define (traverse/count slow fast p1 p2)
+  (cdr (traverse slow fast p1 p2 0)))
+
+; Finds the meeting point, cycle root, and length of cycle
+(define (cycle-detection init)
+  (define meeting
+    (traverse/result next next/next init init))
+  (define root
+    (traverse/result next next init meeting))
+  (define length
+    (traverse/count next identity root root))
+  (list meeting root length))
