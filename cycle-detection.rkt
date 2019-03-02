e2f6ca24ac91a79309d7249f12a82c73d7b40395
Kimserey <kimserey.lam@gmail.com>
2019-03-02 08:48:59 +0000

    more

--- cycle-detection.rkt
+++ cycle-detection.rkt
@@ -18,41 +18,41 @@
       [_ (find (cdr nodes) parent)]))
   (find nodes parent))
 
-(define (next/next x) (next (next x)))
+(define (next-next x) (next (next x)))
 
 ; Traverses a list at the speed specified
-(define (traverse slow fast p1 p2 steps)
+(define (list-traverse slow fast p1 p2 steps)
   (let ([p1 (slow p1)]
         [p2 (fast p2)])
     (cond
       [(= p1 p2) (cons p1 steps)]
-      [else (traverse slow fast p1 p2 (+ 1 steps))])))
+      [else (list-traverse slow fast p1 p2 (+ 1 steps))])))
 
 ; Extracts result from traverse
-(define (traverse/result slow fast p1 p2)
-  (car (traverse slow fast p1 p2 0)))
+(define (list-traverse/result slow fast p1 p2)
+  (car (list-traverse slow fast p1 p2 0)))
 
 ; Extracts the count of iteration from traverse 
-(define (traverse/count slow fast p1 p2)
-  (cdr (traverse slow fast p1 p2 0)))
+(define (list-traverse/count slow fast p1 p2)
+  (cdr (list-traverse slow fast p1 p2 0)))
 
 ; Finds the meeting point, cycle root, and length of cycle
-(define (cycle-detection init)
+(define (cycle init)
   
   ; Step 1: Traverses linked list with tortoise i and hare 2i
   ; to find meeting point.
-  (define meeting
-    (traverse/result next next/next init init))
+  (define cycle-meet
+    (list-traverse/result next next-next init init))
 
   ; Step 2: Move hare back to beginning and traverse
   ; to find cycle root at same speed i.
-  (define root
-    (traverse/result next next meeting init))
+  (define cycle-root
+    (list-traverse/result next next cycle-meet init))
 
   ; Step 3: Keep tortoise in place and move hare
   ; on the cycle to execute one full circle to find
   ; the length.
-  (define length
-    (traverse/count identity next root root))
+  (define cycle-length
+    (list-traverse/count identity next cycle-root cycle-root))
   
-  (list meeting root length))
+  (list cycle-meet cycle-root cycle-length))
