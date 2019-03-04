621e916c37bb5ca67173a58322dc12647f40269d
Kimserey <kimserey.lam@gmail.com>
2019-03-04 09:10:45 +0000

    huffman

---
+++ huffman-trees.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (append l1 l2)
+  (if (null? l1)
+      l2
+      (cons (car l1) (append (cdr l1) l2))))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+         right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
