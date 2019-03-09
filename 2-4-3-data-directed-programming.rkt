fc1b7f8f6a208d1c7dbcc8d5e5d905ec157b6621
Kimserey <kimserey.lam@gmail.com>
2019-03-09 08:30:07 +0000

    Completed data-directed

--- 2-4-3-data-directed-programming.rkt
+++ 2-4-3-data-directed-programming.rkt
@@ -124,4 +124,18 @@
 ; Generic Selectors
 
 (define (real-part z)
-  (apply-generic '
+  (apply-generic 'real-part z))
+(define (imag-part z)
+  (apply-generic 'imag-part z))
+(define (magnitude z)
+  (apply-generic 'magnitude z))
+(define (angle z)
+  (apply-generic 'angle z))
+
+; Constructors
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
