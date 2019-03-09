5123ca93a71739f18b0092c447c17b3fb2760d3f
Kimserey <kimserey.lam@gmail.com>
2019-03-09 06:56:53 +0000

    rectangular and polar form for creating complex number

--- 2-4-tagged-data.rkt
+++ 2-4-tagged-data.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(define (square x) (* x x))
+
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
 
@@ -8,4 +10,63 @@
       (car datum)
       (error "Bad tagged datum:
               TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum:
+              CONTENTS" datum)))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
+
+; Rectangular form
+
+(define (real-part-rectangular z) (car z))
+(define (imag-part-rectangular z) (cdr z))
+
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z))
+           (square (imag-part-rectangular z)))))
+
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z) (real-part-rectangular z)))
+
+(define (make-from-real-imag-rectangular x y)
+  (attach-tag
+   'rectangular
+   (cons x y)))
+
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag
+   'rectangular
+   (cons (* r (cos a))
+         (* r (sin a)))))
+
+; Polar form
+
+(define (real-part-polar z)
+  (* (magnitude-polar z)
+     (cos (angle-polar z))))
+
+(define (imag-part-polar z)
+  (* (magnitude-polar z)
+     (sin (angle-polar z))))
+
+(define (magnitude-polar z) (car z))
+(define (angle-polar z) (cdr z))
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag
+   'polar
+   (cons (sqrt (+ (square x) (square y)))
+         (atan y x))))
+
+(define (make-from-mag-ang-polar r a)
+  (attach-tag
+   'polar
+   (cons r a)))
 
