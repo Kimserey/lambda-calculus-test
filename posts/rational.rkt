a4011847f3674b8ad0c504db2399ffc8a809baf1
Kimserey <kimserey.lam@gmail.com>
2019-03-11 09:22:42 +0000

    more

--- posts/rational.rkt
+++ posts/rational.rkt
@@ -1,40 +1,69 @@
 #lang racket
 
-(define (tag x) (cons 'num x))
 
-(define (value x)
-  (if (num? x)
-      (cdr x)
-      (error "Value provided isn't of type 'num:"
-             x)))
-
-(define (num? . xs)
+(define (num-type? type . xs)
   (if (null? xs)
       #t
       (and (pair? (car xs))
-           (equal? 'num (caar xs))
-           (apply num? (cdr xs)))))
+           (equal? type (caar xs))
+           (apply num-type? type (cdr xs)))))
 
+; numbers
+
+(define (make-num x)
+  (define (tag x) (cons 'num x))
+  (tag x))
+
+(define (value x)
+  (if (pair? x)
+      (cdr x)
+      (error "Value provided isn't tagged:"
+             x)))
+
+(define (num? . xs)
+  (apply num-type? 'num xs))
+  
 (define (add x y) 
   (if (num? x y)
-      (tag (+ (value x) (value y)))
+      (make-num (+ (value x) (value y)))
       (error "Values provided aren't numbers of type 'num:"
              x y)))
 
 (define (sub x y) 
   (if (num? x y)
-      (tag (- (value x) (value y)))
+      (make-num (- (value x) (value y)))
       (error "Values provided aren't numbers of type 'num:"
              x y)))
 
 (define (mul x y) 
   (if (num? x y)
-      (tag (* (value x) (value y)))
+      (make-num (* (value x) (value y)))
       (error "Values provided aren't numbers of type 'num:"
              x y)))
 
 (define (div x y) 
   (if (num? x y)
-      (tag (/ (value x) (value y)))
+      (make-num (/ (value x) (value y)))
       (error "Values provided aren't numbers of type 'num:"
              x y)))
+
+; rationals
+
+(define (make-rat n d) 
+  (define (tag x) (cons 'rational x))
+  (tag (cons n d)))
+
+(define (rat? . xs)
+  (apply num-type? 'rational xs))
+
+(define (numer x)
+  (if (rat? x)
+      (car (value x))
+      (error "Value provided isn't a rational number:"
+             x)))
+
+(define (denom x)
+  (if (rat? x)
+      (cdr (value x))
+      (error "Value provided isn't a rational number:"
+             x)))
