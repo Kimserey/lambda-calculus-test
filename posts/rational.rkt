00b697de5f7de3febfcca70e13c30f7a70a3fc12
Kimserey <kimserey.lam@gmail.com>
2019-03-12 09:06:21 +0000

    more

--- posts/rational.rkt
+++ posts/rational.rkt
@@ -49,9 +49,14 @@
 
 ; rationals
 
+(define (gcd a b)
+  (let ([rest (modulo a b)])
+    (if (zero? rest) b (gcd b rest))))
+
 (define (make-rat n d) 
   (define (tag x) (cons 'rational x))
-  (tag (cons n d)))
+  (let ([g (gcd n d)])
+    (tag (cons (/ n g) (/ d g)))))
 
 (define (rat? . xs)
   (apply num-type? 'rational xs))
@@ -67,3 +72,32 @@
       (cdr (value x))
       (error "Value provided isn't a rational number:"
              x)))
+
+
+(define (add-rat r1 r2)
+  (if (rat? r1 r2)
+      (make-rat
+        (+ (* (numer r1) (denom r2)) (* (numer r2) (denom r1)))
+        (* (denom r1) (denom r2)))
+      (error "Values provided aren't rationals:"
+             r1 r2)))
+
+(define (sub-rat r1 r2)
+  (if (rat? r1 r2)
+      (make-rat
+        (- (* (numer r1) (denom r2)) (* (numer r2) (denom r1)))
+        (* (denom r1) (denom r2)))
+      (error "Values provided aren't rationals:"
+             r1 r2)))
+
+(define (mul-rat r1 r2)
+  (if (rat? r1 r2)
+      (make-rat
+        (* (numer r1) (numer r2))
+        (* (denom r1) (denom r2)))
+      (error "Values provided aren't rationals:"
+             r1 r2)))
+
+(define (div-rat r1 r2)
+  (mul-rat r1 (make-rat (denom r2) (numer r1))))
+
