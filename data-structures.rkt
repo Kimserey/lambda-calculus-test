6b5fb2392a0405e46cf44e77dfc4a972899ff23f
Kimserey <kimserey.lam@gmail.com>
2019-03-16 12:06:11 +0000

    queue and stack

--- data-structures.rkt
+++ data-structures.rkt
@@ -1,17 +1,27 @@
 #lang racket
 
+; Prelude
+
+(define (append ls x)
+  (if (null? ls)
+      (list x)
+      (cons (car ls) (append (cdr ls) x))))
+
 ; Stack implementation
 
 (define (make-stack initial)
   (let ([stack (list initial)])
+
     (define (push x)
       (set! stack (cons x stack)))
+    
     (define (pop)
       (if (pair? stack)
-          (begin
+          (let ([x (car stack)])
             (set! stack (cdr stack))
-            (car stack))
-          (error "Stack is empty")))
+            x)
+          #f))
+    
     (define (dispatch m)
       (cond [(eq? m 'push) push]
             [(eq? m 'pop) pop]))
@@ -21,3 +31,32 @@
   ((stack 'push) x))
 (define (pop stack)
   ((stack 'pop) stack))
+
+; Queue implementation
+
+(define (make-queue)
+  (let ([queue '()])
+    
+    (define (enqueue x)
+      (set! queue (cons x queue)))
+    
+    (define (dequeue)
+      (define (pop-last new-queue rest)
+        (cond
+          [(null? rest) #f]
+          [(null? (cdr rest))
+           (let ([last (car rest)])
+             (set! queue new-queue)
+             last)]
+          [else (pop-last (append new-queue (car rest)) (cdr rest))]))
+      (pop-last '() queue))
+    
+    (define (dispatch m)
+      (cond [(eq? m 'enqueue) enqueue]
+            [(eq? m 'dequeue) dequeue]))
+     dispatch))
+
+(define (enqueue! queue x)
+  ((queue 'enqueue) x))
+(define (dequeue! queue)
+  ((queue 'dequeue)))
