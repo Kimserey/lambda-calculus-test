e07fc6fd51d8cfa2ff36aa1960e1788287563e0e
Kimserey <kimserey.lam@gmail.com>
2019-03-24 10:59:22 +0000

    constant space queue with assignment

--- 3-3-1-mutable-structures.rkt
+++ 3-3-1-mutable-structures.rkt
@@ -24,3 +24,55 @@
   ((z 'set-cdr!) v)
   z)
 
+; Queue with pointers
+; Constant complexity
+
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      #f
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ([new-pair (cons item '())])
+    (cond
+      [(empty-queue? queue)
+       (set-front-ptr! queue new-pair)
+       (set-rear-ptr! queue new-pair)
+       queue]
+      [else
+       (set-cdr! (rear-ptr queue) new-pair)
+       (set-rear-ptr! queue new-pair)
+       queue])))
+
+(define (delete-queue! queue)
+  (cond
+    [(empty-queue? queue) #f]
+    [else
+     (let ([front (front-ptr queue)])
+       (set-front-ptr! queue (cdr front))
+       (car front))]))
+
+(define (print-queue queue)
+  (define (print items)
+    (if (null? items)
+        'end
+        (begin
+          (displayln (car items))
+          (print (cdr items))
+          'end)))
+  (print (front-ptr queue)))
