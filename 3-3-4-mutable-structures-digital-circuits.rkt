ef95394f27694bf57777a83bf7acb42bf5fe6a60
Kimserey <kimserey.lam@gmail.com>
2019-03-25 09:53:21 +0000

    added wire

--- 3-3-4-mutable-structures-digital-circuits.rkt
+++ 3-3-4-mutable-structures-digital-circuits.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-; Prelude
+; Prelude with mutable list and queue
 
 (define (cons x y)
   (define (set-x! v) (set! x v))
@@ -24,6 +24,46 @@
 (define (set-cdr! z v)
   ((z 'set-cdr!) v)
   z)
+
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      #f
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ([new-pair (cons item '())])
+    (cond
+      [(empty-queue? queue)
+       (set-front-ptr! queue new-pair)
+       (set-rear-ptr! queue new-pair)
+       queue]
+      [else
+       (set-cdr! (rear-ptr queue) new-pair)
+       (set-rear-ptr! queue new-pair)
+       queue])))
+
+(define (delete-queue! queue)
+  (cond
+    [(empty-queue? queue) #f]
+    [else
+     (let ([front (front-ptr queue)])
+       (set-front-ptr! queue (cdr front))
+       (car front))]))
 
 ; Logical Gates
 
@@ -58,13 +98,14 @@
     [(= s 1) 0]
     [else (error "Invalid signal" s)]))
 
-(define (and-gate a1 a2 ouput)
+(define (and-gate a1 a2 output)
   (define (and-action-procedure)
     (let ([new-value (logical-and
                       (get-signal a1)
                       (get-signal a2))])
       (after-delay and-gate-delay (λ () (set-signal! output new-value)))))
-  (add-action! input and-action-procedure)
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
   'ok)
 
 (define (logical-and a1 a2)
@@ -78,7 +119,8 @@
                       (get-signal a1)
                       (get-signal a2))])
       (after-delay or-gate-delay (λ () (set-signal! output new-value)))))
-  (add-action! input or-action-procedure)
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
   'ok)
 
 (define (logical-or a1 a2)
@@ -95,7 +137,8 @@
       (if (not (= signal-value new-value))
           (begin
             (set! signal-value new-value)
-            (call-each action-procedures)))
+            (call-each action-procedures))
+          #f)
       'done)
     (define (accept-action-procedure! proc)
       (set! action-procedures (cons proc action-procedures))
@@ -118,7 +161,7 @@
   (wire 'get-signal))
 (define (set-signal! wire new-value)
   ((wire 'set-signal!) new-value))
-(define (accept-action-procedure! wire proc)
+(define (add-action! wire proc)
   ((wire 'accept-action-procedure!) proc))
 
 ; Agenda
@@ -142,17 +185,71 @@
 (define (segment-time s) (car s))
 (define (segment-queue s) (cdr s))
 
-(define (make-agenda) (cons 0 '()))
-(define (current-time agenda) (car agenda))
+(define (make-agenda)
+  (cons 0 '()))
+(define (current-time agenda)
+  (car agenda))
 (define (set-current-time! agenda time)
   (set-car! agenda time))
-(define (segments agenda) (cdr agenda))
+(define (segments agenda)
+  (cdr agenda))
 (define (set-segments! agenda segments)
   (set-cdr! agenda segments))
 (define (first-segment agenda)
   (car (segments agenda)))
-(define (res-segments agenda)
+(define (rest-segments agenda)
   (cdr (segments agenda)))
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ([q (make-queue)])
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments)) action)
+        (let ([rest (cdr segments)])
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment
+                      time
+                      action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ([segments (segments agenda)])
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment
+                time
+                action)
+                segments)
+         (add-to-segments! segments))
+        #f)))
+
+(define (remove-first-agenda-item! agenda)
+  (let ([q (segment-queue (first-segment agenda))])
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda))
+        #f)))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty: FIRST-AGENDA-ITEM")
+      (let ([first-seg (first-segment agenda)])
+        (set-current-time!
+         agenda
+         (segment-time first-seg))
+        (front-queue
+         (segment-queue first-seg)))))
+         
 
 ; Probes
 
@@ -176,3 +273,4 @@
 (define input-2 (make-wire))
 (define sum (make-wire))
 (define carry (make-wire))
+
