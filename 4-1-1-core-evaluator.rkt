7928fb1443a448a9cedc0248b430322392905470
Kimserey <kimserey.lam@gmail.com>
2019-04-19 08:20:25 +0100

    Added part of the evaluator functions

--- 4-1-1-core-evaluator.rkt
+++ 4-1-1-core-evaluator.rkt
@@ -47,3 +47,37 @@
           (procedure-environment procedure))]
         [else
          (error "Unknown procedure type: APPLY" procedure)]))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+; Evaluation of compound sequence returns the last expression result
+; from the list of expressions.
+(define (eval-sequence exprs env)
+  (cond [(last-exp? exps)
+         (eval (first-exp exps) env)]
+        [else
+         (eval (first-exp exps) env)
+         (eval-sequence (rest-exps exps) env)]))
+
+(define (eval-assignment exp env)
+  (set-variable-value!
+   (assignment-variable exp)
+   (eval (assignment-value exp) env)
+   env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable!
+    (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
