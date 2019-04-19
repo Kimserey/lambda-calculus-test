9e8439d45192bbd1d762444374809c9de65cfd55
Kimserey <kimserey.lam@gmail.com>
2019-04-19 08:11:19 +0100

    apply

--- 4-1-1-core-evaluator.rkt
+++ 4-1-1-core-evaluator.rkt
@@ -31,3 +31,19 @@
                  env))]
         [else
          (error "Unknown expression type: EVAL" exp)]))
+
+(define (apply procedure arguments)
+  (cond [(primitive-procedure? procedure)
+         (apply-primitive-procedure
+          procedure
+          arguments)]
+        [(compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           procedure)
+          arguments
+          (procedure-environment procedure))]
+        [else
+         (error "Unknown procedure type: APPLY" procedure)]))
