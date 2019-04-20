ca3c2749d2649e6500029fdfb75a6de209e36fd6
Kimserey <kimserey.lam@gmail.com>
2019-04-20 18:57:52 +0100

    more

--- 4-1-1-core-evaluator.rkt
+++ 4-1-1-core-evaluator.rkt
@@ -189,8 +189,13 @@
 
 ; Procedure application
 (define (application? exp) (pair? exp))
+
 (define (operator exp) (car exp))
+
 (define (operands exp) (cdr exp))
+
 (define (no-operands? ops) (null? ops))
+
 (define (first-operand ops) (car ops))
+
 (define (rest-operands ops) (cdr ops))
