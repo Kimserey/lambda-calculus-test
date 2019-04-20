6656fa7da16a29f084525d19e0274233a1832e21
Kimserey <kimserey.lam@gmail.com>
2019-04-20 18:12:53 +0100

    more

--- 4-1-1-core-evaluator.rkt
+++ 4-1-1-core-evaluator.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+; **
+; Eval and Apply
+; **
+
 (define (eval exp env)
   (cond [(self-evaluating? exp)
          ex]
@@ -54,11 +58,13 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
+; Conditional
 (define (eval-if exp env)
   (if (true? (eval (if-predicate exp) env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
 
+; Sequences
 ; Evaluation of compound sequence returns the last expression result
 ; from the list of expressions.
 (define (eval-sequence exprs env)
@@ -68,6 +74,7 @@
          (eval (first-exp exps) env)
          (eval-sequence (rest-exps exps) env)]))
 
+; Assignments and definitions
 (define (eval-assignment exp env)
   (set-variable-value!
    (assignment-variable exp)
@@ -81,3 +88,82 @@
     (eval (definition-value exp) env)
     env)
   'ok)
+
+; **
+; Expressions
+; **
+
+; Numbers and strings self evaluate
+(define (self-evaluating? exp)
+  (cond [(number? exp) true]
+        [(string? exp) true]
+        [else false]))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+; Quotations are list starting with 'quote
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp)
+  (cadr exp))
+
+; Assignments start with 'set!
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp)
+  (cadr exp))
+
+(define (assignment-value exp)
+  (caddr exp))
+
+; Definitions start with 'define
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda
+       (cdadr exp)
+       (cddr exp))))
+
+; Lambdas
+(define (lambda? exp)
+  (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp)
+  (cadr exp))
+
+(define (lambda-body exp)
+  (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+; Conditionals
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predicate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
